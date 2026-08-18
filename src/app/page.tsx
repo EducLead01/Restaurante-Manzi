b226f232ee8ab7c291fe65127bd21f33155e1055
EducLead01 <planejamentoeduclead@gmail.com>
2026-08-18 00:30:22 -0300

    Move seção Diferenciais (Por que o Manzi) para o final do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
         <Identidade />
         <Cardapio />
         <Experiencia />
-        <Diferenciais />
         <Novidades />
         <Galeria />
+        <Diferenciais />
       </main>
       <Footer />
       <WhatsappButton />
